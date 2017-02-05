fef653caa3aba993249eec315a9dc6dabee44d63
Ольга Дикун <dicoun@gmail.com>
2017-02-05 13:52:44 +0300

    Doubly linked list task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,9 +1,35 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+
+        var NewNode = new Node();
+        NewNode.data = data;
+        if(this.length == null){
+                this._head = NewNode;
+                this._tail = NewNode;
+        }
+        else{
+            if(this._head.next == null){
+                this._head.next = NewNode;
+                NewNode.prev = this._head;
+                this._tail = NewNode;
+            }
+            else{
+                this._tail.next = NewNode;
+                NewNode.prev = this._tail;
+                this._tail = NewNode;
+            }
+        }
+
+
+
+
+    }
 
     head() {}
 
